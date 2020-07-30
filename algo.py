0f137fbb58f2fa1a0ca5b7410d050ea02b0bd929
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-07-31 03:45:05 +0530

    Create algo.py

---
+++ algo.py
@@ -0,0 +1,186 @@
+class ISS_CAM1(BaseCAM):
+
+    """
+        ISS-CAM - 1
+        Integrating over the upsampled activation map
+
+    """
+
+    def __init__(self, model_dict):
+        super().__init__(model_dict)
+
+    def forward(self, input, class_idx=None, retain_graph=False):
+        b, c, h, w = input.size()
+        
+        # predication on raw input
+        logit = self.model_arch(input)
+        
+        if class_idx is None:
+            predicted_class = logit.max(1)[-1]
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            predicted_class = torch.LongTensor([class_idx])
+            score = logit[:, class_idx].squeeze()
+        
+        logit = F.softmax(logit)
+
+        if torch.cuda.is_available():
+          predicted_class= predicted_class.cuda()
+          score = score.cuda()
+          logit = logit.cuda()
+
+        self.model_arch.zero_grad()
+        score.backward(retain_graph=retain_graph)
+        activations = self.activations['value']
+        b1, k, u, v = activations.size()
+
+        
+        score_saliency_map = torch.zeros((1, 1, h, w))
+
+        if torch.cuda.is_available():
+          activations = activations.cuda()
+          score_saliency_map = score_saliency_map.cuda()
+
+
+        with torch.no_grad():
+          for i in range(k):
+
+              # upsampling
+              saliency_map = torch.unsqueeze(activations[:, i, :, :], 1)
+              
+              saliency_map = F.interpolate(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+ 
+              if saliency_map.max() == saliency_map.min():
+                continue
+              
+              x = saliency_map               
+
+              score_list = []
+              newmap_list = []
+
+              c = 0.1
+              
+              for i in range(1, 11):
+
+                new_map = x * c * i
+
+                newmap_list.append(new_map)
+               
+                output = self.model_arch(new_map * input)
+                output = F.softmax(output)
+                score = output[0][predicted_class]
+                score_list.append(score)
+              
+              
+              score = max(score_list) - min(score_list)
+              score_saliency_map +=  score * saliency_map
+                
+        score_saliency_map = F.relu(score_saliency_map)
+        score_saliency_map_min, score_saliency_map_max = score_saliency_map.min(), score_saliency_map.max()
+
+        if score_saliency_map_min == score_saliency_map_max:
+            return None
+
+        score_saliency_map = (score_saliency_map - score_saliency_map_min).div(score_saliency_map_max - score_saliency_map_min).data
+
+        return score_saliency_map
+
+    def __call__(self, input, class_idx=None, retain_graph=False):
+        return self.forward(input, class_idx, retain_graph)
+
+
+class ISS_CAM2(BaseCAM):
+
+    """
+        ISS-CAM - 2
+        Integrating over the normalized input mask        
+    """
+
+    def __init__(self, model_dict):
+        super().__init__(model_dict)
+
+    def forward(self, input, class_idx=None, retain_graph=False):
+        b, c, h, w = input.size()
+        
+        # predication on raw input
+        logit = self.model_arch(input)
+        
+        if class_idx is None:
+            predicted_class = logit.max(1)[-1]
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        else:
+            predicted_class = torch.LongTensor([class_idx])
+            score = logit[:, class_idx].squeeze()
+        
+        logit = F.softmax(logit)
+
+        if torch.cuda.is_available():
+          predicted_class= predicted_class.cuda()
+          score = score.cuda()
+          logit = logit.cuda()
+
+        self.model_arch.zero_grad()
+        score.backward(retain_graph=retain_graph)
+        activations = self.activations['value']
+        b1, k, u, v = activations.size()
+
+        
+        score_saliency_map = torch.zeros((1, 1, h, w))
+
+        if torch.cuda.is_available():
+          activations = activations.cuda()
+          score_saliency_map = score_saliency_map.cuda()
+
+        
+
+        with torch.no_grad():
+          for i in range(k):
+
+              # upsampling
+              saliency_map = torch.unsqueeze(activations[:, i, :, :], 1)
+              
+              saliency_map = F.interpolate(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+
+              if saliency_map.max() == saliency_map.min():
+                continue
+
+              # normalize to 0-1
+              norm_saliency_map = (saliency_map - saliency_map.min()) / (saliency_map.max() - saliency_map.min())              
+
+              
+              x = input * norm_saliency_map
+
+              #if i % 50 == 0 and i < 300:
+              #  visualize(input.cpu(), x.type(torch.FloatTensor).cpu())         
+
+              score_list = []
+              newmap_list = []
+
+              c = 0.1
+              
+              for i in range(1, 11):
+
+                new_map = x * c * i
+
+                newmap_list.append(new_map)
+                
+                output = self.model_arch(new_map)
+                output = F.softmax(output)
+                score = output[0][predicted_class]
+                score_list.append(score)
+                
+              score = max(score_list) - min(score_list)
+              score_saliency_map +=  score * saliency_map
+                
+        score_saliency_map = F.relu(score_saliency_map)
+        score_saliency_map_min, score_saliency_map_max = score_saliency_map.min(), score_saliency_map.max()
+
+        if score_saliency_map_min == score_saliency_map_max:
+            return None
+
+        score_saliency_map = (score_saliency_map - score_saliency_map_min).div(score_saliency_map_max - score_saliency_map_min).data
+
+        return score_saliency_map
+
+    def __call__(self, input, class_idx=None, retain_graph=False):
+        return self.forward(input, class_idx, retain_graph)
